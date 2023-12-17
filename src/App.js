9fb4a5c39e21e10f13f12b2a3b15d987ac8059a7
Anuj Kumar <anujk182002@gmail.com>
2023-12-17 22:28:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './components/Home';
+import Header from './components/Header';
+function App() {
+  return (
+  <div classnName='app'>
+  
+  <Router>
+   <Switch>
+     
+      <Route path='/'>
+        <Header />
+         <Home />
+       </Route>
+
+       
+
+
+    </Switch>
+
+
+   </Router>
+
+
+  </div>
+  );
+}
+
+export default App;
