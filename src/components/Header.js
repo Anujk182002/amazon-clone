11b7ee4aa669f80cb0931fea78d4c6ca51238764
Anuj Kumar <anujk182002@gmail.com>
2023-12-18 14:54:13 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,19 @@ import SearchIcon from '@material-ui/icons/Search'
 import { ShoppingBasket } from '@material-ui/icons'
 
 import { useStateValue } from '../providers/StateProvider'
+import { Link } from 'react-router-dom';
+import { auth } from '../firebase'
+
 
 function Header() {
 
   const [{ basket, user}, dispatch] = useStateValue();
+
+  const handleAuthentication = () => {
+    if(user) {
+      auth.signOut();
+    }
+  }
 
   return (
     <div className='header'>
@@ -26,10 +35,12 @@ function Header() {
 
         <div className='header__nav'>
           
-          <div className='header__option'>
+          <Link to='/login' >
+          <div className='header__option' onClick={handleAuthentication}>
            <span className='header__optionLineOne'>Hello</span>
            <span className='header__optionLineTwo'>Sign In</span>
           </div>
+          </Link>
 
           <div className='header__option'>
            <span className='header__optionLineOne'>Return</span>
